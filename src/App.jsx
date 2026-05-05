0a44534986cc77e5a3169570c0523f9203f61f0e
VINAYLAKAMANAHALLI <vinaylakamanahalli@gmail.com>
2026-05-05 14:05:40 +0530

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-9xl text-white bg-blue-900'>App</div>
+    <div className='text-9xl text-white bg-blue-900'>checking </div>
   )
 }
 
